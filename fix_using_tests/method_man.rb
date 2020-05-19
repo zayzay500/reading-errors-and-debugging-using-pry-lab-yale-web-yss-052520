6e5baaaa889bd2d486a719818663a1b222baed3e
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-19 00:39:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,9 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  binding.pry
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
