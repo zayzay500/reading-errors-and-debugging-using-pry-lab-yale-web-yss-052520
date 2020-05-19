a95f25d1b8bd745d4f574ac6bab1fdf68d0be385
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-19 00:41:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,7 +5,7 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   binding.pry
   "#{player1} is better than #{player2}"
 end
